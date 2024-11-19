839f56013bf3c43f5308a2ba2fb0be3c65b7db46
dimasradityapermana <144466230+dimasradityapermana@users.noreply.github.com>
2024-11-19 16:45:04 +0800

    admin_pengaturan

--- admin_pengaturan.php
+++ admin_pengaturan.php
@@ -1,5 +1,39 @@
 <?php
 require "koneksi.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // tabel admin
+    $nama_admin = $_POST['nama_admin'];
+    $sandi_admin = $_POST['sandi'];
+    $gambar_admin = $_FILES['gambar_admin'];
+
+    // tabel website
+    $judul = $_POST['judul'];
+    $email = $_POST['email'];
+    $telepon = $_POST['telepon'];
+    $alamat = $_POST['alamat'];
+    // $deskripsi_tentang = $_POST['deskripsi_tentang'];
+    // $visi
+    // $misi
+    $instagram = $_POST['instagram'];
+    $facebook = $_POST['facebook'];
+    $youtube = $_POST['youtube'];
+    $twitter = $_POST['twitter'];
+    $deskripsi_footer = $_POST['deskripsi_footer'];
+    $logo_web = $_FILES['logo_web'];
+
+    $admin_upload_dir = 'images/admin/';
+    $website_upload_dir = 'images/website/';
+
+    move_uploaded_file($gambar_admin['tmp_name'], $admin_upload_dir . basename($gambar_admin['name']));
+    move_uploaded_file($logo_web['tmp_name'], $website_upload_dir . basename($logo_web['name']));
+
+    $query_admin = "UPDATE admin SET nama_admin='$nama_admin', sandi='$sandi_admin', gambar_admin='{$gambar_admin['name']}' WHERE id=1";
+    mysqli_query($conn, $query_admin);
+
+    $query_website = "UPDATE website SET judul='$judul', deskripsi_footer='$deskripsi_footer', instagram='$instagram', twitter='$twitter', facebook='$facebook', youtube='$youtube', logo_web='{$logo_web['name']}' WHERE id=1";
+    mysqli_query($conn, $query_website);
+}
 ?>
 
 <!DOCTYPE html>
@@ -23,90 +57,77 @@ require "koneksi.php";
         <?php include "navbar.php"; ?>
     </header>
 
-        <div class="container-admin-pengaturan">
-            <div class="page-title">
-                <h1>Pengaturan</h1>
-            </div>
-            <div class="slice-container">
-                <div class="admin-pengaturan-slice-left">
-                    <div class="page-title-2">
-                        <h2>Website</h2>
-                    </div>
-                    <div>
-                        <form action="proses-adminpengaturan.php" method="post" enctype="multipart/form-data">
-                            <div>
-                                <label for="deskripsi-footer-admin">Nama Pengguna Admin</label>
-                                <input class="form-field" type="text" id="deskripsi-footer-admin" name="deskripsi-footer-admin" required></input>
-                            </div>
-                            <div>
-                                <label for="link-instagram-admin">Sandi Admin</label>
-                                <input class="form-field" id="link-instagram-admin" name="link-instagram-admin" required></input>
-                            </div>
-                            <div>
-                                <label for="judul-website-admin">Judul Website</label>
-                                <input class="form-field" type="email" id="judul-website-admin" name="judul-website-admin" required></input>
-                            </div>
-                            <div>
-                                <label for="email-admin">Email</label>
-                                <input class="form-field" type="text" id="email-admin" name="email-admin" required></input>
-                            </div>
-                            <div>
-                                <label for="nomor-admin">No Telepon</label>
-                                <input class="form-field" id="nomor-admin" name="nomor-admin" required></input>
-                            </div>
-                            <div>
-                                <label for="profil-admin">Profil Admin</label>
-                                <input class="form-field" type="file" id="profil-admin" name="profil-admin" accept="image/*" required></input>
-                            </div>
-                            <div>
-                                <label for="logo-website-admin">Logo Website</label>
-                                <input class="form-field" type="file" id="logo-website-admin" name="logo-website-admin" accept="image/*" required></input>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+    <main>
+        <div class="container-hero">
+            <h1>Pengaturan</h1>
+        </div>
 
-                <div class="admin-pengaturan-slice-right">
-                    <div class="page-title-2">
-                        <h2>Footer</h2>
-                    </div>
-                    <div>
-                        <form action="proses-adminpengaturan.php" method="post" enctype="multipart/form-data">
-                            <div>
-                                <label for="deskripsi-footer-admin">Deskripsi Footer<Footer></Footer></label>
-                                <textarea class="form-field" type="text" id="deskripsi-footer-admin" name="deskripsi-footer-admin" required></textarea>
-                            </div>
-                            <div>
-                                <label for="link-instagram-admin">Link Instagram</label>
-                                <input class="form-field" id="link-instagram-admin" name="link-instagram-admin" required></input>
-                            </div>
-                            <div>
-                                <label for="link-twitter-admin">Link Twitter</label>
-                                <input class="form-field" type="email" id="link-twitter-admin" name="link-twitter-admin" required></input>
-                            </div>
-                            <div>
-                                <label for="link-facebook-admin">Link Facebook</label>
-                                <input class="form-field" type="text" id="link-facebook-admin" name="link-facebook-admin" required></input>
-                            </div>
-                            <div>
-                                <label for="link-youtube-admin">Link Youtube</label>
-                                <input class="form-field" id="link-youtube-admin" name="link-youtube-admin" required></input>
-                            </div>
-                        </form>
-                    </div>
+        <div class="container-pengaturan">
+            <div class="header-pengaturan">
+                <div class="test-warna">
+                    <h2>Website</h2>
+                </div>
+                <div class="test-warna">
+                    <h2>Footer</h2>
                 </div>
             </div>
+        </div>
 
-            <div class="btn-admin-tentang">
-                <button class="btn-batal-admin">Batal</button>
-                <button class="btn-simpan-admin">Simpan</button>
-            </div>
+        <div>
+            <form action="" method="POST" enctype="multipart/form-data">
+
+
+                <div>
+                    <label for="nama_admin">Nama Admin</label>
+                    <input type="text" id="nama_admin" name="nama_admin" required><br>
+
+                    <label for="sandi">Sandi Admin</label>
+                    <input type="password" id="sandi" name="sandi" required><br>
+
+                    <label for="judul">Judul Website</label>
+                    <input type="text" id="judul" name="judul" required><br>
+
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" required><br>
+
+                    <label for="telepon">No Telepon</label>
+                    <input type="text" id="telepon" name="telepon" required><br>
+
+                    <label for="alamat">Alamat</label>
+                    <input type="text" id="alamat" name="alamat" required><br>
+                </div>
+
+                <div>
+                    <label for="deskripsi_footer">Deskripsi Footer</label>
+                    <textarea id="deskripsi_footer" name="deskripsi_footer" required></textarea><br>
+                    
+                    <label for="instagram">Link Instagram</label>
+                    <input type="url" id="instagram" name="instagram" required><br>
+                    
+                    <label for="twitter">Link Twitter</label>
+                    <input type="url" id="twitter" name="twitter" required><br>
+                    
+                    <label for="facebook">Link Facebook</label>
+                    <input type="url" id="facebook" name="facebook" required><br>
+                    
+                    <label for="youtube">Link YouTube</label>
+                    <input type="url" id="youtube" name="youtube" required><br>
+                </div>
+                
+                <label for="gambar_admin">Gambar Admin</label>
+                <input type="file" id="gambar_admin" name="gambar_admin" required><br>
+
+                <label for="logo_web">Logo Website</label>
+                <input type="file" id="logo_web" name="logo_web" required><br>
+
+                <input type="submit" value="Submit">
+            </form>
         </div>
     </main>
 
-    <footer>
-        <?php include "footer.php"; ?>
 
+    <footer>
+        <!-- <?php include "footer.php"; ?> -->
     </footer>
 
 </body>
